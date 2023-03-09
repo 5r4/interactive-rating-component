ab904fa64e7a41e46230645cbd74daa7c80c6897
Daxter <47326283+5r4@users.noreply.github.com>
2023-03-10 00:36:43 +0300

    React Codes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Rating } from "./components/rating";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className="continer">
+        <div className="card">
+          <Rating />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
